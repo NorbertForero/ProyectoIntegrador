9d278b448a0ec4fb2421d6b86ab775d0e6d1b6ae
Norbert Forero <nsforero@sanmateo.edu.co>
2020-05-01 23:23:41 -0500

    Front Flujo1

--- app/src/main/java/com/proyectointegrador/MainActivity.java
+++ app/src/main/java/com/proyectointegrador/MainActivity.java
@@ -2,20 +2,47 @@ package com.proyectointegrador;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.proyectointegrador.ui.main.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
+    private Button flujo1;
+    private Button flujo2;
+    private Button flujo3;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        flujo1 = findViewById(R.id.button1);
+        flujo2 = findViewById(R.id.button2);
+        flujo3 = findViewById(R.id.button3);
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.container, MainFragment.newInstance())
                     .commitNow();
         }
+
+        flujo1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), newRegister.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/proyectointegrador/newRegister.java
@@ -0,0 +1,14 @@
+package com.proyectointegrador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class newRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_register);
+    }
+}
