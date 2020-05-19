26c330b6e75166fa059a6b758184e50aaf99fbd4
Andrés Pérez <andresperezfirigua@gmail.com>
2020-05-19 11:57:26 -0500

    Actuaización flujo 2

--- app/src/main/java/com/proyectointegrador/activityConsultarRegistro.java
+++ app/src/main/java/com/proyectointegrador/activityConsultarRegistro.java
@@ -1,17 +1,22 @@
 package com.proyectointegrador;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.List;
 
 public class activityConsultarRegistro extends AppCompatActivity {
 
     private Button editar;
+    private EditText textConsultar;
+    private Button search;
+    private RecyclerView resultado;
 
 
     @Override
@@ -20,6 +25,10 @@ public class activityConsultarRegistro extends AppCompatActivity {
         setContentView(R.layout.activity_consultar_registro);
 
         editar = findViewById(R.id.buttonEditar);
+        textConsultar= findViewById(R.id.editTextConsultar);
+        search = findViewById(R.id.buttonBuscar);
+        resultado = findViewById(R.id.recyclerviewResultado);
+
 
         editar.setOnClickListener(new View.OnClickListener() {
             @Override
